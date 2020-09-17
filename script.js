5d4e511248bdf9e73d061c29bceb22b54e903e3d
CaroS <carolyn.mary@hotmail.com>
2020-09-16 23:49:44 -0400

    commit 3

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 $(document).ready(function () {
+  var usersSearch = "";
   var historyList = JSON.parse(localStorage.getItem("usersSearch")) || []; // Array of cities searched
+  // var cityName = historyList[0];
+  var cityID = [""];
+  var apiKey = "&appid=72d6f50b9321f45148779dadfd9505b5";
+
 
   // store user input to local storage
   $("#searchBtn").click(function () {
-    var usersSearch = $(this).siblings("#searchInput").val().trim(); // identifies and saves the users search input
+    usersSearch = $(this).siblings("#searchInput").val().trim(); // identifies and saves the users search input
     historyList.unshift(usersSearch); // adds users' search to historyList
     localStorage.setItem("usersSearch", JSON.stringify(historyList)); // save usersEvent to localStorage
-    createHistoryButtons();
     $(this).siblings("#searchInput").val(null); // clear input field
+    createHistoryButtons();
+    cityNameToID();
+    getCurrentWeather();
   });
 
   // display the search history in the history list
@@ -31,27 +38,51 @@ $(document).ready(function () {
     localStorage.removeItem("usersSearch");
     location.reload();
   });
+  var cityID = [""];
+  // Convert city name to city id# 
+  function cityNameToID() {
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + historyList[0] + apiKey;
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function (response) {
+      console.log(response);
+      var newID = response.id;
+      cityID.unshift(newID);
+      // console.log("inside NameToID " + newID);
+    });
+  };
+  cityNameToID();
 
+  // GET current weather 
+  function getCurrentWeather() {
+    console.log("getCurr working")
+    // cityID = toString(cityID);
+    console.log("inside getCurrent " + cityID);
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?id=" + cityID[0] + apiKey;
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function (response) {
+      console.log(response);
+    //   $("#currentCity").append().text(response.name);
+    //   $("#currentDate").append().text(response.dt);
+    //   $("#currentIcon").append().text(response.weather[0].icon);
+    //   $("#currentTemp").append().text("Temperature: " + response.main.temp);
+    //   $("#currentHum").append().text("Humidity: " + response.main.humidity);
+    //   $("#currentWind").append().text("Wind Speed: " + response.wind.speed);
+    // });
+  };
 
+  // DISPLAY current weather (history[0])
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  // var currentCity = response.name;
+  // var currentDate = response.dt;
+  // var currentIcon = response.weather[0].icon;
+  // var currentTemp = response.main.temp;
+  // var currentHum = response.main.humidity;
+  // var currentWind = response.wind.speed;
+  // $("#currentIcon").append("http://openweathermap.org/img/wn/" + currentIcon + "@2x.png");
 
 
 
